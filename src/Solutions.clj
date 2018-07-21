31fce3f825bf80c2c8af088e693614ffd3feed29
ioannakok <ioannakok@hotmail.com>
2018-07-21 11:02:07 +0100

    First 22 exercises

--- src/Solutions.clj
+++ src/Solutions.clj
@@ -1,1 +1,73 @@
 (ns Solutions)
+
+;; #1: Nothing but the Truth
+true
+
+;; #2: Simple Math
+4
+
+;; #3: Intro to Strings
+"HELLO WORLD"
+
+;; #4: Intro to Lists
+:a :b :c
+
+;; #5: Lists: conj
+'(1 2 3 4)
+
+;; #6: Intro to Vectors
+:a :b :c
+
+;; #7: Vectors: conj
+[1 2 3 4]
+
+;; #8: Intro to Sets
+#{:a :b :c :d}
+
+;; #9: Sets: conj
+2
+
+;; #10: Intro Maps
+20
+
+;; #11: Maps: conj
+{:b 2}
+
+;; #12: Intro to Sequences
+3
+
+;; #13: Sequences: rest
+[20 30 40]
+
+;; #14: Intro to Functions
+8
+
+;; #15: Double Down
+(fn double-num [x] (* x 2))
+
+;; #16: Hello World
+(fn greeting [x] (str "Hello, " x "!"))
+
+;; #17: Sequences: map
+'(6 7 8)
+
+;; #18: Sequences: filter
+'(6 7)
+
+;; #19: Last Element
+(comp first reverse)
+
+;; #20: Penultimate Element
+(comp first (comp rest reverse))
+
+;; #21: Nth element
+(fn [coll n]
+  (if (zero? n)
+    (first coll)
+    (recur (rest coll) (dec n))))
+
+
+;; #22: Count a sequence
+(fn [coll]
+  (reduce + (map (fn [x]
+                   x 1) coll)))
